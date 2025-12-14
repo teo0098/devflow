8e52322ff77b511143ffc1859caae83d6516ab9f
teo0098 <teodor.tkaczyk0407@gmail.com>
2025-12-14 16:22:46 +0100

    feat: add assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 const Page = () => {
-  return <h1 className="text-2xl">Welcome to ultimate next course</h1>;
+  return (
+    <>
+      <h1 className="h1-bold font-space-grotesk">Welcome to ultimate next course 232</h1>
+      <h1 className="h1-bold">Welcome to ultimate next course 222s (INTER2)</h1>
+    </>
+  );
 };
 
 export default Page;
