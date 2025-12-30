f42d1b7bdb9810f425286b3b82cefa4e1e5bf9ac
teo0098 <teodor.tkaczyk0407@gmail.com>
2025-12-30 10:56:11 +0100

    feat: implement account by provider

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,39 @@
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { ValidationError, NotFoundError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { providerAccountId } = await req.json();
+
+  try {
+    const validatedData = AccountSchema.partial().safeParse({ providerAccountId });
+
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    await dbConnect();
+
+    const account = await Account.findOne({ providerAccountId });
+
+    if (!account) {
+      throw new NotFoundError("Account");
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: account,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
