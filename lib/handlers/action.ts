5179da12fbb6dab928d46b4488262d3693a1c7c7
teo0098 <teodor.tkaczyk0407@gmail.com>
2026-01-02 13:25:24 +0100

    feat: implement action handler

---
+++ lib/handlers/action.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { ZodError } from "zod";
+import { ZodSchema } from "zod/v3";
+import { UnauthorizedError, ValidationError } from "../http-errors";
+import { Session } from "next-auth";
+import { auth } from "@/auth";
+import dbConnect from "../mongoose";
+
+type ActionOptions<T> = {
+  params?: T;
+  schema?: ZodSchema<T>;
+  authorize?: boolean;
+};
+
+async function action<T>({ params, schema, authorize = false }: ActionOptions<T>) {
+  if (schema && params) {
+    try {
+      schema.parse(params);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return new ValidationError(error.flatten().fieldErrors);
+      } else {
+        return new Error("Schema validation failed");
+      }
+    }
+  }
+
+  let session: Session | null = null;
+
+  if (authorize) {
+    session = await auth();
+
+    if (!session) {
+      return new UnauthorizedError();
+    }
+  }
+
+  await dbConnect();
+
+  return {
+    params,
+    session,
+  };
+}
+
+export default action;
