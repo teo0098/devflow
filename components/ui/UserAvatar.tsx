0f985b9d50159326c3294d8f2df352f1c1f510ff
teo0098 <teodor.tkaczyk0407@gmail.com>
2026-01-10 17:57:18 +0100

    feat: add users page

--- components/ui/UserAvatar.tsx
+++ components/ui/UserAvatar.tsx
@@ -3,9 +3,8 @@ import Link from "next/link";
 import React from "react";
 
 import ROUTES from "@/constants/routes";
-
-import { Avatar, AvatarFallback } from "./avatar";
 import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback } from "./avatar";
 
 interface Props {
   id: string;
@@ -31,14 +30,13 @@ const UserAvatar = ({
 
   return (
     <Link href={ROUTES.PROFILE(id)}>
-      <Avatar className={className}>
+      <Avatar className={cn("relative", className)}>
         {imageUrl ? (
           <Image
             src={imageUrl}
             alt={name}
             className="object-cover"
-            width={36}
-            height={36}
+            fill
             quality={100}
           />
         ) : (
