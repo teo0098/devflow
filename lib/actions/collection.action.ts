c61225cf8839630310ad124d188456125e47c74e
teo0098 <teodor.tkaczyk0407@gmail.com>
2026-01-11 12:27:42 +0100

    feat: implement collections page

--- lib/actions/collection.action.ts
+++ lib/actions/collection.action.ts
@@ -107,6 +107,7 @@ export async function getSavedQuestions(
   const validationResult = await action({
     params,
     schema: PaginatedSearchParamsSchema,
+    authorize: true,
   });
 
   if (validationResult instanceof Error) {
