f08b8b50cfa46d6c15b9f1ac5a0f774058bb4337
teo0098 <teodor.tkaczyk0407@gmail.com>
2026-01-24 16:48:54 +0100

    feat: use react cache function for getQuestion server action

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -23,6 +23,7 @@ import {
 import ROUTES from "@/constants/routes";
 import { revalidatePath } from "next/cache";
 import dbConnect from "../mongoose";
+import { cache } from "react";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -203,7 +204,7 @@ export async function editQuestion(
   }
 }
 
-export async function getQuestion(
+export const getQuestion = cache(async function getQuestion(
   params: GetQuestionParams
 ): Promise<ActionResponse<IQuestionDoc>> {
   const validationResult = await action({
@@ -235,7 +236,7 @@ export async function getQuestion(
   } catch (error) {
     return handleError(error) as ErrorResponse;
   }
-}
+});
 
 export async function getQuestions(
   params: PaginatedSearchParams
